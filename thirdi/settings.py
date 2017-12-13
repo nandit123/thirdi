c04c2afdfaddfb850e169fe5198bcd58b43985ac
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2017-12-14 00:56:22 +0530

    heroku static change1

--- thirdi/settings.py
+++ thirdi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$ms#-46ugmw#vm-!^^s3ao@w(#-3gsvwoob@#^0ehvxuftfm-g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,13 +118,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+#
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "resources"),)
+# STATIC_ROOT = (os.path.join(BASE_DIR, 'static'))
 
-STATIC_URL = '/static/'
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'blog/'),
-    os.path.join(BASE_DIR, 'extras/'),
-    os.path.join(BASE_DIR, "resources"),
+    os.path.join(BASE_DIR, 'static'),
 )
