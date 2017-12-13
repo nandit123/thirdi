0e1177f9bfecc00e17405624de8f06a24689f2c7
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2017-12-14 00:12:10 +0530

    Added static

--- thirdi/settings.py
+++ thirdi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$ms#-46ugmw#vm-!^^s3ao@w(#-3gsvwoob@#^0ehvxuftfm-g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "resources"),)
-STATIC_ROOT = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'blog/'),
+    os.path.join(BASE_DIR, 'extras/'),
+    os.path.join(BASE_DIR, "resources"),
+)
